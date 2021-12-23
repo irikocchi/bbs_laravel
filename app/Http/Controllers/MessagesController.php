3242cfd1ccb2825c6951bc9a4057debd62d5e846
Nanao Ikari <ju.to25nawo@gmail.com>
2021-12-24 00:35:04 +0900

    標準validationのカスタマイズ、withメソッドを用いたフラッシュメッセージ使用とold関数を用いた入力値の再代入完了

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -27,16 +27,19 @@ class MessagesController extends Controller
          // Messageモデルを使って、MySQLのmessagesテーブルから全データ取得
         $messages = Message::all();
         
-        // フラッシュメッセージをセッションから取得
-        $flash_message = session('flash_message');
-        // セッション情報の破棄
-        session()->forget('flash_message');
+        // // フラッシュメッセージをセッションから取得
+        // $flash_message = session('flash_message');
+        // // セッション情報の破棄
+        // session()->forget('flash_message');
         
-        // エラーメッセージにnullをセット
-        $errors = null;
+        // // エラーメッセージにnullをセット
+        // $errors = null;
 
         // 連想配列のデータを3セット（viewで引き出すキーワードと値のセット）引き連れてviewを呼び出す
-        return view('messages.index', compact('messages', 'flash_message', 'errors'));
+        // return view('messages.index', compact('messages', 'flash_message', 'errors'));
+        
+        // 連想配列のデータを1セット（viewで引き出すキーワードと値のセット）引き連れてviewを呼び出す
+        return view('messages.index', compact('messages'));
     }
 
        /**
@@ -46,27 +49,11 @@ class MessagesController extends Controller
      */
     public function create()
     {
-       // 空のメッセージインスタンスを作成
+        // 空のメッセージインスタンスを作成
         $message = new Message();
         
-        // セッションにメッセージが保存されていれば
-        if(session('message')){
-            // セッションからメッセージ取得
-            $message = session('message');
-            // セッション情報の破棄
-            session()->forget('message');
-        }
-        
-        // フラッシュメッセージをnullにセット
-        $flash_message = null;
-        
-        // エラーメッセージをセッションから取得
-        $errors = session('errors');
-        // セッション情報の破棄
-        session()->forget('errors');
-        
-        // 連想配列のデータを3セット（viewで引き出すキーワードと値のセット）引き連れてviewを呼び出す
-        return view('messages.create', compact('message', 'flash_message', 'errors'));
+        // 連想配列のデータを1セット（viewで引き出すキーワードと値のセット）引き連れてviewを呼び出す
+        return view('messages.create', compact('message'));
     }
 
 
@@ -78,6 +65,143 @@ class MessagesController extends Controller
      */
     public function store(Request $request)
     {
+        // // 入力された値を取得
+        // $name = $request->input('name');
+        // $title = $request->input('title');
+        // $body = $request->input('body');
+        // // 画像ファイル情報の取得だけ特殊
+        // $file =  $request->image;
+        
+        // // 画像ファイルが選択されていれば
+        // if ($file) { 
+        
+        //     // 現在時刻ともともとのファイル名を組み合わせてランダムなファイル名作成
+        //     $image = time() . $file->getClientOriginalName();
+        //     // アップロードするフォルダ名取得
+        //     $target_path = public_path('uploads/');
+
+        // } else { // ファイルが選択されていなければ
+        //     $image = null;
+        // }
+        
+        // // 空のメッセージインスタンスを作成
+        // $message = new Message();
+        
+        // // 入力された値をセット
+        // $message->name = $name;
+        // $message->title = $title;
+        // $message->body = $body;
+        // $message->image = $image;
+        
+        // // 入力エラーチェック
+        // $errors = $message->validate();
+
+        // // 入力エラーが1つもなければ
+        // if(count($errors) === 0){
+        //     // 画像アップロード処理
+        //     $file->move($target_path, $image);
+            
+        //     // メッセージインスタンスをデータベースに保存
+        //     $message->save();
+            
+        //     // セッションにflash_messageを保存
+        //     session(['flash_message' => '新規投稿が成功しました']);
+            
+        //     // indexアクションにリダイレクト
+        //     return redirect('/');
+            
+        // }else{
+        //     // セッションに、入力したメッセージインスタンス と errors保存
+        //     session(['errors' => $errors, 'message' => $message]);
+            
+        //     // createアクションにリダイレクト
+        //     return redirect('/messages/create');
+        // }
+        
+        // validation
+        $this->validate($request, [
+            'name' => 'required',
+            'title' => 'required',
+            'body' => 'required',
+            'image' => [
+                'required',
+                'file',
+                'mimes:jpeg,jpg,png'
+            ]
+        ]);
+        
+        // 入力された値を取得
+        $name = $request->input('name');
+        $title = $request->input('title');
+        $body = $request->input('body');
+        // 画像ファイル情報の取得だけ特殊
+        $file =  $request->image;
+        
+        // 現在時刻ともともとのファイル名を組み合わせてランダムなファイル名作成
+        $image = time() . $file->getClientOriginalName();
+        // アップロードするフォルダ名取得
+        $target_path = public_path('uploads/');
+        // 画像アップロード処理
+        $file->move($target_path, $image);
+
+        // 空のメッセージインスタンスを作成
+        $message = new Message();
+        
+        // 入力された値をセット
+        $message->name = $name;
+        $message->title = $title;
+        $message->body = $body;
+        $message->image = $image;
+
+        // メッセージインスタンスをデータベースに保存
+        $message->save();
+        
+        // セッションにフラッシュメッセージを保存しながら、indexアクションにリダイレクト
+        return redirect('/')->with('flash_message', '新規投稿が成功しました');
+           
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Message $message)
+    {
+        // 連想配列のデータを1セット（viewで引き出すキーワードと値のセット）引き連れてviewを呼び出す
+        return view('messages.show', compact('message'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Message $message)
+    {
+        
+ // 連想配列のデータを1セット（viewで引き出すキーワードと値のセット）引き連れてviewを呼び出す
+        return view('messages.edit', compact('message'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Message $message)
+    {
+        // validation
+        $this->validate($request, [
+            'name' => 'required',
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+        
         // 入力された値を取得
         $name = $request->input('name');
         $title = $request->input('title');
@@ -92,99 +216,24 @@ class MessagesController extends Controller
             $image = time() . $file->getClientOriginalName();
             // アップロードするフォルダ名取得
             $target_path = public_path('uploads/');
+            // 画像アップロード処理
+            $file->move($target_path, $image);
 
-        } else { // ファイルが選択されていなければ
-            $image = null;
+        } else { // ファイルが選択されていなければ元の値を保持
+            $image = $message->image;
         }
         
-        // 空のメッセージインスタンスを作成
-        $message = new Message();
-        
-        // 入力された値をセット
+        // 入力されたでインスタンス情報を更新
         $message->name = $name;
         $message->title = $title;
         $message->body = $body;
         $message->image = $image;
-        
-        // 入力エラーチェック
-        $errors = $message->validate();
 
-        // 入力エラーが1つもなければ
-        if(count($errors) === 0){
-            // 画像アップロード処理
-            $file->move($target_path, $image);
-            
-            // メッセージインスタンスをデータベースに保存
-            $message->save();
-            
-            // セッションにflash_messageを保存
-            session(['flash_message' => '新規投稿が成功しました']);
-            
-            // indexアクションにリダイレクト
-            return redirect('/');
-            
-        }else{
-            // セッションに、入力したメッセージインスタンス と errors保存
-            session(['errors' => $errors, 'message' => $message]);
-            
-            // createアクションにリダイレクト
-            return redirect('/messages/create');
-        }
-           
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Message  $message
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Message $message)
-    {
-         // フラッシュメッセージをセッションから取得
-        $flash_message = session('flash_message');
-        // セッション情報の破棄
-        session()->forget('flash_message');
+        // データベースを更新
+        $message->save();
         
-        // エラーメッセージをnullにセット
-        $errors = null;
-        
-        // 連想配列のデータを3セット（viewで引き出すキーワードと値のセット）引き連れてviewを呼び出す
-        return view('messages.show', compact('message', 'flash_message', 'errors'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Message  $message
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Message $message)
-    {
-        
-        // フラッシュメッセージをnullにセット
-        $flash_message = null;
-        
-        // エラーメッセージをセッションから取得
-        $errors = session('errors');
-        // セッション情報の破棄
-        // Laravelでセッションから指定の変数を削除(定型文)
-        session()->forget('errors');
-        
-        // 連想配列のデータを3セット（viewで引き出すキーワードと値のセット）引き連れてviewを呼び出す
-        return view('messages.edit', compact('message', 'flash_message', 'errors'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Message  $message
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Message $message)
-    {
-        //
+        // フラッシュメッセージを保存しながらshowアクションにリダイレクト
+        return redirect('/messages/' . $message->id)->with('flash_message', 'id: ' . $message->id . 'の投稿の更新が成功しました');
     }
 
     /**
@@ -195,12 +244,9 @@ class MessagesController extends Controller
      */
     public function destroy(Message $message)
     {
-         // 該当メッセージをデータベースから削除
+        // 該当メッセージをデータベースから削除
         $message->delete();
-        // セッションにflash_message をセット
-        session(['flash_message' => 'id: ' . $message->id . 'の投稿を削除しました']);
-        
-        // indexアクションにリダイレクト
-        return redirect('/');
+        // フラッシュメッセージを保存しながらshowアクションにリダイレクト
+        return redirect('/')->with('flash_message', 'id: ' . $message->id . 'の投稿を削除しました');
     }
 }
--- resources/views/commons/errors.blade.php
+++ resources/views/commons/errors.blade.php
@@ -1,7 +1,13 @@
-@if($errors !== null)
+             @if($errors->any())
+            <!-- //$errors->any()は、変数$errorsの中にエラーメッセージがあるかどうかチェックを行うメソッドです。 -->
+            <!-- //エラーメッセージがあればtrue、なければfalseを戻します。 -->
+
             <ul class="row mt-2">
-            @foreach($errors as $error)  
+            @foreach($errors->all() as $error)  
                 <li class="text-center col-sm-12">{{ $error }}</li>
             @endforeach
             </ul>
+            <!-- //$errors->all()にはバリデーションの中のチェックで発生したエラー情報が配列として保持しています。 -->
+            <!-- //foreachを使用することで1つ1つのメッセージを取り出すことができます。 -->
             @endif
+           
